24ea775d9c3a6affdc06742303df90c307127c2e
junior-od <opeyemi.oduberu@gmail.com>
2022-07-26 01:03:01 +0100

    error page done

---
+++ app/src/main/java/com/adyen/android/assignment/ui/ErrorActivity.kt
@@ -0,0 +1,12 @@
+package com.adyen.android.assignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adyen.android.assignment.R
+
+class ErrorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+    }
+}
