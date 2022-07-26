97d78ece6c15eb4f4ad80eb7dd1b0a0daca472ea
junior-od <opeyemi.oduberu@gmail.com>
2022-07-26 01:55:03 +0100

    detail screen done

---
+++ app/src/main/java/com/adyen/android/assignment/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.adyen.android.assignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adyen.android.assignment.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
